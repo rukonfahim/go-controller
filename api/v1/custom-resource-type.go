b441148c1d0800605da96bc7e9610630a78b1733
rukonfahim@gmail.com <rukonfahim@gmail.com>
2023-03-23 03:43:42 +0600

    go controller fix package error

--- api/v1/custom-resource-type.go
+++ api/v1/custom-resource-type.go
@@ -2,6 +2,7 @@ package v1
 
 import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime" // add this line
 )
 
 // MyResourceSpec defines the desired state of MyResource
@@ -25,4 +26,47 @@ type MyResourceList struct {
 	metav1.TypeMeta `json:",inline"`
 	metav1.ListMeta `json:"metadata,omitempty"`
 	Items           []MyResource `json:"items"`
+}
+
+// DeepCopyObject creates a deep copy of this object and returns the new object.
+func (in *MyResource) DeepCopyObject() runtime.Object {
+	if in == nil {
+		return nil
+	}
+	out := new(MyResource)
+	in.DeepCopyInto(out)
+	return out
+}
+
+// DeepCopyInto creates a deep copy of this object and stores it in the specified object.
+func (in *MyResource) DeepCopyInto(out *MyResource) {
+	*out = *in
+	out.TypeMeta = in.TypeMeta
+	in.ObjectMeta.DeepCopyInto(&out.ObjectMeta)
+	out.Spec = in.Spec
+}
+
+
+// DeepCopyObject creates a deep copy of this object and returns the new object.
+func (in *MyResourceList) DeepCopyObject() runtime.Object {
+	if in == nil {
+		return nil
+	}
+	out := new(MyResourceList)
+	in.DeepCopyInto(out)
+	return out
+}
+
+// DeepCopyInto creates a deep copy of this object and stores it in the specified object.
+func (in *MyResourceList) DeepCopyInto(out *MyResourceList) {
+	*out = *in
+	out.TypeMeta = in.TypeMeta
+	in.ListMeta.DeepCopyInto(&out.ListMeta)
+	if in.Items != nil {
+		in, out := &in.Items, &out.Items
+		*out = make([]MyResource, len(*in))
+		for i := range *in {
+			(*in)[i].DeepCopyInto(&(*out)[i])
+		}
+	}
 }
